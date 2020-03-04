592215640e80d17eeac52035389f3cf31f5bfdff
root <1258252786@qq.com>
2020-03-04 16:15:14 +0800

    广度优先遍历

---
+++ src/main/java/top/vergessen/bfs/GraphBFS.java
@@ -0,0 +1,52 @@
+package top.vergessen.bfs;
+
+import top.vergessen.graph.Graph;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class GraphBFS {
+
+    private Graph G;
+    private boolean[] visited;
+    private ArrayList<Integer> order = new ArrayList<>();
+
+    public GraphBFS(Graph G){
+
+        this.G = G;
+        visited = new boolean[G.getV()];
+        for (int v = 0; v < G.getV(); v++){
+            if (!visited[v])
+                bfs(v);
+        }
+    }
+
+    private void bfs(int v) {
+
+        Queue<Integer> queue = new LinkedList<>();
+        queue.add(v);
+        visited[v] = true;
+        while (!queue.isEmpty()){
+            int w = queue.remove();
+            order.add(w);
+
+            for(int s : G.adj(w)){
+                if (!visited[s]) {
+                    queue.add(s);
+                    visited[s] = true;
+                }
+            }
+        }
+    }
+
+    public Iterable<Integer> order(){
+        return order;
+    }
+
+    public static void main(String[] args) {
+        Graph graph = new Graph("src/main/g.txt");
+        GraphBFS graphBFS = new GraphBFS(graph);
+        System.out.println(graphBFS.order());
+    }
+}
