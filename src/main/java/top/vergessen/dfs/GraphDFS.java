44e6abe4e870fc91dbb650501f31fb46ddc87538
root <1258252786@qq.com>
2020-03-04 18:28:30 +0800

    DFS非递归实现

--- src/main/java/top/vergessen/dfs/GraphDFS.java
+++ src/main/java/top/vergessen/dfs/GraphDFS.java
@@ -3,12 +3,15 @@ package top.vergessen.dfs;
 import top.vergessen.graph.Graph;
 
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Stack;
 
 public class GraphDFS {
 
     private Graph G;
     private boolean[] visited;
     private ArrayList<Integer> pre = new ArrayList<>();
+    private ArrayList<Integer> pre2 = new ArrayList<>();
     private ArrayList<Integer> post = new ArrayList<>();
 
     public GraphDFS(Graph G){
@@ -18,6 +21,11 @@ public class GraphDFS {
         for (int v = 0; v < G.getV(); v++)
             if (!visited[v])
                 dfs(v);
+
+        Arrays.fill(visited,false);
+        for (int v = 0; v < G.getV(); v++)
+            if (!visited[v])
+                dfs2(v);
     }
 
     // 时间复杂度 O(V+E)
@@ -31,8 +39,31 @@ public class GraphDFS {
         post.add(v);          // 后序遍历
     }
 
+    //深度优先遍历非递归
+    private void dfs2(int s) {
+
+        Stack<Integer> stack = new Stack<>();
+        stack.push(s);
+        visited[s] = true;
+
+        while (!stack.isEmpty()) {
+            int v = stack.pop();
+            pre2.add(v);
+
+            for (int w : G.adj(v)) {
+                if (!visited[w]) {
+                    stack.push(w);
+                    visited[w] = true;
+                }
+            }
+        }
+    }
+
     public Iterable<Integer> pre(){
         return pre;
+    }
+    public Iterable<Integer> pre2(){
+        return pre2;
     }
 
     public Iterable<Integer> post(){
@@ -44,5 +75,6 @@ public class GraphDFS {
         GraphDFS graphDFS = new GraphDFS(graph);
         System.out.println(graphDFS.pre());
         System.out.println(graphDFS.post());
+        System.out.println(graphDFS.pre2());
     }
 }
